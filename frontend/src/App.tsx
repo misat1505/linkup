98c5384edc1272bd1fd7bbf83c1ea1c235e8d2c1
misat1505 <misat2002@gmail.com>
2024-07-01 12:17:58 +0200

    configured shadcn

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "./components/ui/avatar";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <Avatar>
+      <AvatarImage src="https://github.com/shadcn.png" />
+      <AvatarFallback>CN</AvatarFallback>
+    </Avatar>
+  );
 }
 
 export default App;
