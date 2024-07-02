e3da2ef8283454b37af486dfd052ca9df3500ebd
misat1505 <misat2002@gmail.com>
2024-07-02 16:50:36 +0200

    tests for login validation rules

---
+++ backend/tests/validators/auth.validators.test.ts
@@ -0,0 +1,81 @@
+import { ValidationChain } from "express-validator";
+import express from "express";
+import { validate } from "../../src/middlewares/validate";
+import { loginRules } from "../../src/validators/auth.validator";
+import request from "supertest";
+
+const createTestServer = (validations: ValidationChain[]) => {
+  const app = express();
+  app.use(express.json());
+  app.post("/test", validate(validations), (req, res) => {
+    res.status(200).send("Validation passed");
+  });
+  return app;
+};
+
+describe("Auth validators", () => {
+  describe("Login rules", () => {
+    it("passes with correct data", async () => {
+      const app = createTestServer(loginRules);
+      const res = await request(app).post("/test").send({
+        login: "login1",
+        password: "pass1",
+      });
+
+      expect(res.statusCode).toBe(200);
+      expect(res.text).toBe("Validation passed");
+    });
+
+    it("fails with no data", async () => {
+      const app = createTestServer(loginRules);
+      const res = await request(app).post("/test").send({});
+
+      expect(res.statusCode).toBe(400);
+      expect(res.body.errors).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({ msg: "Login must be provided." }),
+          expect.objectContaining({ msg: "Password must be provided." }),
+        ])
+      );
+    });
+
+    it("fails with data too short", async () => {
+      const app = createTestServer(loginRules);
+      const res = await request(app).post("/test").send({
+        login: "logi",
+        password: "pass",
+      });
+
+      expect(res.statusCode).toBe(400);
+      expect(res.body.errors).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({
+            msg: "Login must be between 5 and 50 characters long.",
+          }),
+          expect.objectContaining({
+            msg: "Password must be at least 5 characters long.",
+          }),
+        ])
+      );
+    });
+
+    it("fails with data too long", async () => {
+      const app = createTestServer(loginRules);
+      const res = await request(app)
+        .post("/test")
+        .send({
+          login: "l".repeat(51),
+          password: "pass1",
+        });
+
+      expect(res.statusCode).toBe(400);
+      expect(res.body.errors).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({
+            msg: "Login must be between 5 and 50 characters long.",
+          }),
+        ])
+      );
+    });
+  });
+});
