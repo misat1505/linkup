3b8b58918275a091350571acb4a422d3d1b4cb17
misat1505 <misat2002@gmail.com>
2024-07-08 13:27:37 +0200

    UserService

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -3,7 +3,7 @@ import { Hasher } from "../lib/Hasher";
 import { User } from "../models/User";
 import { JwtHandler } from "../lib/JwtHandler";
 import { createFilename } from "../lib/utils/file";
-import { db } from "../lib/DatabaseConnector";
+import { UserService } from "../services/UserService";
 
 export const signupUser = async (req: Request, res: Response) => {
   const { firstName, lastName, login, password } = req.body;
@@ -11,26 +11,13 @@ export const signupUser = async (req: Request, res: Response) => {
 
   const hashedPassword = Hasher.hash(password);
 
-  const isLoginTaken =
-    (
-      await db.executeQuery("SELECT COUNT(*) FROM Users WHERE login = ?", [
-        login,
-      ])
-    )[0]["COUNT(*)"] > 0;
+  const isLoginTaken = await UserService.isLoginTaken(login);
 
   if (isLoginTaken) {
     return res.status(409).json({ message: "Login already taken." });
   }
 
-  const result = await db.executeQuery(
-    "INSERT INTO Users (login, password, first_name, last_name, photoURL) VALUES (?, ?, ?, ?, ?);",
-    [login, hashedPassword, firstName, lastName, file]
-  );
-
-  const userId = result.insertId;
-
-  const user: User = {
-    id: userId,
+  const userData: Omit<User, "id"> = {
     firstName,
     lastName,
     login,
@@ -38,27 +25,25 @@ export const signupUser = async (req: Request, res: Response) => {
     photoURL: file,
   };
 
-  const jwt = JwtHandler.encode({ userId }, { expiresIn: "1h" });
+  const id = await UserService.insertUser(userData);
+
+  const user: User = { id, ...userData };
+  const jwt = JwtHandler.encode({ userId: id }, { expiresIn: "1h" });
   res.cookie("token", jwt, { httpOnly: true });
   return res.status(201).json({ user });
 };
 
 export const loginUser = async (req: Request, res: Response) => {
   const { login, password } = req.body;
-
   const hashedPassword = Hasher.hash(password);
 
-  const result = await db.executeQuery(
-    "SELECT id FROM Users WHERE login = ? AND password = ?;",
-    [login, hashedPassword]
-  );
-  const userData = result[0];
+  const user = await UserService.loginUser(login, hashedPassword);
 
-  if (!userData) {
+  if (!user) {
     return res.status(401).json({ message: "Invalid login or password." });
   }
 
-  const jwt = JwtHandler.encode({ userId: userData.id }, { expiresIn: "1h" });
+  const jwt = JwtHandler.encode({ userId: user.id }, { expiresIn: "1h" });
   res.cookie("token", jwt, { httpOnly: true });
   return res.status(200).json({ message: "Successfully logged in." });
 };
@@ -82,18 +67,11 @@ export const logoutUser = (req: Request, res: Response) => {
 export const getUser = async (req: Request, res: Response) => {
   const userId = req.body.token.userId;
 
-  const result = await db.executeQuery("SELECT * FROM Users WHERE id = ?;", [
-    userId,
-  ]);
+  const user = await UserService.getUser(userId);
 
-  const userData = result[0];
-
-  if (!userData) {
+  if (!user) {
     return res.status(404).json({ message: "User not found." });
   }
-
-  const { first_name: firstName, last_name: lastName, ...rest } = userData;
-  const user = { firstName, lastName, ...rest } as User;
 
   return res.status(200).json({ user });
 };
---
+++ backend/src/services/UserService.ts
@@ -0,0 +1,60 @@
+import { db } from "../lib/DatabaseConnector";
+import { User } from "../models/User";
+
+type DatabaseUser = Omit<User, "firstName" | "lastName"> & {
+  first_name: User["firstName"];
+  last_name: User["lastName"];
+};
+
+export class UserService {
+  static async isLoginTaken(login: User["login"]): Promise<boolean> {
+    const result = await db.executeQuery(
+      "SELECT COUNT(*) FROM Users WHERE login = ?",
+      [login]
+    );
+
+    return result[0]["COUNT(*)"] > 0;
+  }
+
+  static async insertUser(user: Omit<User, "id">): Promise<number> {
+    const { firstName, login, lastName, password, photoURL } = user;
+
+    const result = await db.executeQuery(
+      "INSERT INTO Users (login, password, first_name, last_name, photoURL) VALUES (?, ?, ?, ?, ?);",
+      [login, password, firstName, lastName, photoURL]
+    );
+
+    return result.insertId;
+  }
+
+  static async loginUser(
+    login: User["login"],
+    password: User["password"]
+  ): Promise<User | null> {
+    const result = await db.executeQuery(
+      "SELECT * FROM Users WHERE login = ? AND password = ?;",
+      [login, password]
+    );
+    const userData = result[0];
+    return userData ? this.intoUser(userData as DatabaseUser) : null;
+  }
+
+  static async getUser(id: number): Promise<User | null> {
+    const result = await db.executeQuery("SELECT * FROM Users WHERE id = ?;", [
+      id,
+    ]);
+
+    const user = result[0];
+    return user ? this.intoUser(user as DatabaseUser) : null;
+  }
+
+  static intoUser(databaseUser: DatabaseUser): User {
+    const {
+      first_name: firstName,
+      last_name: lastName,
+      ...rest
+    } = databaseUser;
+    const user = { firstName, lastName, ...rest } as User;
+    return user;
+  }
+}
