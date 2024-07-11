773c7e68f86b70ac02b52595c62128fc71bee49e
misat1505 <misat2002@gmail.com>
2024-07-11 17:25:29 +0200

    better jwt cookie options

--- backend/src/config/jwt-ccokie.ts
+++ backend/src/config/jwt-ccokie.ts
@@ -2,8 +2,5 @@ import { CookieOptions } from "express";
 
 export const jwtCookieOptions: CookieOptions = {
   httpOnly: true,
-  secure: true,
-  sameSite: "none",
-  path: "/",
   maxAge: 60 * 60 * 1000, // 1hr
 };
