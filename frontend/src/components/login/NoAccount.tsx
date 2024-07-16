722addfbfdd6eb8b0f3743e83db8812fde636376
misat1505 <misat2002@gmail.com>
2024-07-16 14:43:12 +0200

    changed <a> to <Link> for login signup links

--- frontend/src/components/login/NoAccount.tsx
+++ frontend/src/components/login/NoAccount.tsx
@@ -1,12 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function NoAccount() {
   return (
     <p className="mt-4 text-sm text-center">
       Don&apos;t have an account?{" "}
-      <a className="underline text-blue-700" href="/signup">
+      <Link className="underline text-blue-700" to="/signup">
         Signup
-      </a>
+      </Link>
     </p>
   );
 }
--- frontend/src/components/signup/AlreadyHaveAccount.tsx
+++ frontend/src/components/signup/AlreadyHaveAccount.tsx
@@ -1,12 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function AlreadyHaveAccount() {
   return (
     <p className="mt-4 text-sm text-center">
       Already have an account?{" "}
-      <a className="underline text-blue-700" href="/login">
+      <Link className="underline text-blue-700" to="/login">
         Login
-      </a>
+      </Link>
     </p>
   );
 }
