d3697ef558fe8dba2e092b002df2aa1edae807dd
misat1505 <misat2002@gmail.com>
2024-07-22 11:25:57 +0200

    updated logo

--- frontend/src/components/login/LoginPageLogo.tsx
+++ frontend/src/components/login/LoginPageLogo.tsx
@@ -1,12 +1,24 @@
 import React from "react";
-import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import { LOGO_PATH } from "../../constants";
+import { Img } from "react-image";
+import { Skeleton } from "../ui/skeleton";
+import { cn } from "../../lib/utils";
 
 export default function LoginPageLogo() {
+  const commonClasses = "mx-auto mb-4 h-64 w-64 rounded-full";
+
   return (
-    <Avatar className="mx-auto mb-4 h-64 w-64">
-      <AvatarImage className="object-cover" src={LOGO_PATH} />
-      <AvatarFallback>Logo</AvatarFallback>
-    </Avatar>
+    <Img
+      className={commonClasses}
+      src={LOGO_PATH}
+      loader={<Skeleton className={commonClasses} />}
+      unloader={
+        <div
+          className={cn("items-center justify-center bg-white", commonClasses)}
+        >
+          Logo
+        </div>
+      }
+    />
   );
 }
