e17121db303ac114beeaae2368adc649205c4d03
misat1505 <misat2002@gmail.com>
2024-07-22 11:13:05 +0200

    updated formfield classnames merge

--- frontend/src/components/common/forms/FormField.tsx
+++ frontend/src/components/common/forms/FormField.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Input } from "../../ui/input";
 import FormFieldError from "./FormFieldError";
+import { cn } from "../../../lib/utils";
 
 type FormFieldProps = React.InputHTMLAttributes<HTMLInputElement> & {
   error?: string | null;
@@ -12,11 +13,13 @@ const FormField = React.forwardRef<HTMLInputElement, FormFieldProps>(
       <div className="w-full">
         <Input
           ref={ref}
-          className={`my-2 w-full border-b-2 border-b-slate-400 bg-transparent p-2 ${
-            error
-              ? "mt-4 ring-2 ring-red-500 ring-offset-2 focus-visible:ring-red-500"
-              : ""
-          }`}
+          className={cn(
+            "my-2 w-full border-b-2 border-b-slate-400 bg-transparent p-2",
+            {
+              "mt-4 ring-2 ring-red-500 ring-offset-2 focus-visible:ring-red-500":
+                error
+            }
+          )}
           {...rest}
         />
         {error && <FormFieldError message={error} />}
