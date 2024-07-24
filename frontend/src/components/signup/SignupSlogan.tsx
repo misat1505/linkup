cf8acdf94248ff2c3b9f5638b149dda676669aa0
misat1505 <misat2002@gmail.com>
2024-07-24 13:48:45 +0200

    signup page done

---
+++ frontend/src/components/signup/SignupSlogan.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function SignupSlogan() {
+  return (
+    <div className="col-span-1 mx-20 flex flex-col justify-center">
+      <h1 className="mt-20 text-center text-9xl font-bold text-white">
+        Join the Network
+      </h1>
+      <p className="mt-32 text-center text-2xl font-semibold">
+        Start your journey today and discover a world of connections and
+        opportunities.
+      </p>
+    </div>
+  );
+}
