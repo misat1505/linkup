d4fd8d45f825d4c5fc03f7afd6749c2249d58fb5
misat1505 <misat2002@gmail.com>
2024-07-25 13:20:56 +0200

    animation for slogans

--- frontend/src/components/login/LoginSlogan.tsx
+++ frontend/src/components/login/LoginSlogan.tsx
@@ -1,16 +1,20 @@
 import React from "react";
+import styles from "../../styles/slogan.module.css";
+import { cn } from "../../lib/utils";
 
 export default function LoginSlogan() {
   return (
     <div className="col-span-1 mx-20 flex flex-col justify-center">
-      <h1
-        className="mt-20 text-9xl font-bold text-white"
-        style={{ textShadow: "0 25px 50px rgba(0, 0, 0, 0.5)" }}
-      >
+      <h1 className={cn("mt-20 text-9xl font-bold text-white", styles.title)}>
         <div className="text-nowrap">Welcome to</div>
         <div className="text-nowrap">Link Up</div>
       </h1>
-      <p className="mt-32 text-center text-2xl font-semibold">
+      <p
+        className={cn(
+          "mt-32 text-center text-2xl font-semibold",
+          styles.description
+        )}
+      >
         Immerse yourself in a social network where connecting with friends,
         sharing your moments, and discovering new communities is just a click
         away.
