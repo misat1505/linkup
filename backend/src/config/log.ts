b7de97d54fa09e880ade0a8c5a09eca7d713cb86
misat1505 <misat2002@gmail.com>
2024-08-06 14:01:02 +0200

    logs every 2 hours

--- backend/src/config/log.ts
+++ backend/src/config/log.ts
@@ -5,6 +5,6 @@ import fs from "fs";
 fs.mkdirSync(path.join(__dirname, "..", "..", "logs"), { recursive: true });
 
 export const accessLogStream = createStream("access.log", {
-  interval: "1d",
+  interval: "2h",
   path: path.join(__dirname, "..", "..", "logs"),
 });
