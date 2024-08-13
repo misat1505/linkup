ceaf7d950646d8ef77aa00af87569a27c83530ee
misat1505 <misat2002@gmail.com>
2024-08-13 11:37:03 +0200

    slogans better balancing

--- frontend/src/components/login/LoginSlogan.tsx
+++ frontend/src/components/login/LoginSlogan.tsx
@@ -1,26 +1,32 @@
 import React from "react";
 import styles from "../../styles/slogan.module.css";
 import { cn } from "../../lib/utils";
-import Balancer from "react-wrap-balancer";
+import { useLocalStorageAnimation } from "../../hooks/useLocalStorageAnimation";
 
 export default function LoginSlogan() {
+  const isAnimating = useLocalStorageAnimation(
+    "login-animation",
+    60 * 60 * 1000
+  );
+
   return (
     <div className="col-span-1 mx-20 flex flex-col justify-center">
-      <h1 className={cn("mt-20 text-9xl font-bold text-white", styles.title)}>
+      <h1
+        className={cn("mt-20 text-9xl font-bold text-white", {
+          [styles.title]: isAnimating
+        })}
+      >
         <div className="text-nowrap">Welcome to</div>
         <div className="text-nowrap">Link Up</div>
       </h1>
       <p
-        className={cn(
-          "mt-32 text-center text-2xl font-semibold",
-          styles.description
-        )}
+        className={cn("mt-32 text-balance text-center text-2xl font-semibold", {
+          [styles.description]: isAnimating
+        })}
       >
-        <Balancer>
-          Immerse yourself in a social network where connecting with friends,
-          sharing your moments, and discovering new communities is just a click
-          away.
-        </Balancer>
+        Immerse yourself in a social network where connecting with friends,
+        sharing your moments, and discovering new communities is just a click
+        away.
       </p>
     </div>
   );
--- frontend/src/components/signup/SignupSlogan.tsx
+++ frontend/src/components/signup/SignupSlogan.tsx
@@ -1,30 +1,31 @@
 import React from "react";
 import styles from "../../styles/slogan.module.css";
 import { cn } from "../../lib/utils";
-import Balancer from "react-wrap-balancer";
+import { useLocalStorageAnimation } from "../../hooks/useLocalStorageAnimation";
 
 export default function SignupSlogan() {
+  const isAnimating = useLocalStorageAnimation(
+    "signup-animation",
+    60 * 60 * 1000
+  );
+
   return (
     <div className="col-span-1 mx-20 flex flex-col justify-center">
       <h1
-        className={cn(
-          "mt-20 text-center text-9xl font-bold text-white",
-          styles.title
-        )}
+        className={cn("mt-20 text-center text-9xl font-bold text-white", {
+          [styles.title]: isAnimating
+        })}
       >
         <div className="text-nowrap">Join the</div>
         <div className="text-nowrap">Network</div>
       </h1>
       <p
-        className={cn(
-          "mt-32 text-center text-2xl font-semibold",
-          styles.description
-        )}
+        className={cn("mt-32 text-balance text-center text-2xl font-semibold", {
+          [styles.description]: isAnimating
+        })}
       >
-        <Balancer>
-          Start your journey today and discover a world of connections and
-          opportunities.
-        </Balancer>
+        Start your journey today and discover a world of connections and
+        opportunities.
       </p>
     </div>
   );
