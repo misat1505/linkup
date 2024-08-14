2bfeba9cfb689dc0a484ab4c7b4e8ddd0fc54aa1
misat1505 <misat2002@gmail.com>
2024-08-14 14:52:29 +0200

    created authorization for creating private chat endpoint

--- backend/src/controllers/chat.controller.ts
+++ backend/src/controllers/chat.controller.ts
@@ -3,7 +3,15 @@ import { ChatService } from "../services/ChatService";
 
 export const createPrivateChat = async (req: Request, res: Response) => {
   try {
-    const { users } = req.body;
+    const {
+      users,
+      token: { userId },
+    } = req.body;
+
+    if (!users.includes(userId))
+      return res
+        .status(401)
+        .json({ message: "Cannot create private chat not belonging to you." });
 
     const chat = await ChatService.getPrivateChatByUserIds(users[0], users[1]);
 
