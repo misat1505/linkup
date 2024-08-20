0615fc5ebe590957870167df37ff53446355dbff
misat1505 <misat2002@gmail.com>
2024-08-20 14:07:51 +0200

    deleted comments

--- frontend/src/components/chats/ChatContent.tsx
+++ frontend/src/components/chats/ChatContent.tsx
@@ -8,40 +8,9 @@ import useChatScroll from "../../hooks/chats/useChatScroll";
 export default function ChatContent() {
   const { messages, isLoading, incomeMessage } = useChatContext();
   const { bottomRef, containerRef, handleScroll } = useChatScroll();
-  // const containerRef = useRef<HTMLDivElement>(null);
-  // const bottomRef = useRef<HTMLDivElement>(null);
-  // const scrollTopRef = useRef<number>(0);
-  // const isInitialMount = useRef<boolean>(true);
-  // const wasAtBottomRef = useRef<boolean>(true);
 
   const styles =
     "flex-grow overflow-hidden rounded-tl-lg bg-slate-100 pt-2 relative";
-
-  // const handleScroll = () => {
-  //   if (containerRef.current) {
-  //     scrollTopRef.current = containerRef.current.scrollTop;
-  //     const container = containerRef.current;
-  //     const atBottom =
-  //       container.scrollHeight - container.scrollTop === container.clientHeight;
-  //     wasAtBottomRef.current = atBottom;
-  //   }
-  // };
-
-  // useLayoutEffect(() => {
-  //   const container = containerRef.current;
-  //   if (container) {
-  //     if (isInitialMount.current) {
-  //       bottomRef.current?.scrollIntoView();
-  //       isInitialMount.current = false;
-  //     } else {
-  //       if (wasAtBottomRef.current) {
-  //         bottomRef.current?.scrollIntoView({ behavior: "smooth" });
-  //       } else {
-  //         container.scrollTop = scrollTopRef.current;
-  //       }
-  //     }
-  //   }
-  // }, [messages]);
 
   if (isLoading) {
     return (
