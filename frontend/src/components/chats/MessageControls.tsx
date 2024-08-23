7cf622d8f4768f088e917c1ea38d9e79a9ea2c7d
misat1505 <misat2002@gmail.com>
2024-08-23 20:57:36 +0200

    messagecontrols tooltip

--- frontend/src/components/chats/MessageControls.tsx
+++ frontend/src/components/chats/MessageControls.tsx
@@ -2,6 +2,12 @@ import { Message } from "../../models/Message";
 import React from "react";
 import { RiReplyFill } from "react-icons/ri";
 import { useChatFooterContext } from "../../contexts/ChatFooterProvider";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger
+} from "../ui/tooltip";
 
 type MessageControlsProps = { message: Message };
 
@@ -11,11 +17,23 @@ export default function MessageControls({ message }: MessageControlsProps) {
   return (
     <div className="hidden group-hover:block">
       <div className="flex items-center gap-x-4">
-        <RiReplyFill
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger>
+              <RiReplyFill
+                size={20}
+                className="text-muted-foreground transition-all hover:cursor-pointer hover:text-slate-400"
+                onClick={() => setResponse(message.id)}
+              />
+            </TooltipTrigger>
+            <TooltipContent>Reply</TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+        {/* <RiReplyFill
           size={20}
           className="text-muted-foreground transition-all hover:cursor-pointer hover:text-slate-400"
           onClick={() => setResponse(message.id)}
-        />
+        /> */}
       </div>
     </div>
   );
