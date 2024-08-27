844e26674d1ac65bb97fc1976e016c7c376469d3
misat1505 <misat2002@gmail.com>
2024-08-27 15:15:31 +0200

    tooltips in navbar

--- frontend/src/components/common/navbar/Navbar.tsx
+++ frontend/src/components/common/navbar/Navbar.tsx
@@ -5,19 +5,30 @@ import NavbarSearch from "./NavbarSearch";
 import NavbarSheet from "./NavbarSheet";
 import { useAppContext } from "../../../contexts/AppProvider";
 import ThemeToggle from "./ThemeToggle";
+import Tooltip from "../Tooltip";
+import { useNavigate } from "react-router-dom";
+import { ROUTES } from "../../../lib/routes";
 
 export default function Navbar() {
   const { user } = useAppContext();
+  const navigate = useNavigate();
   const isLoggedIn = !!user;
 
   return (
     <>
       <header className="fixed z-10 flex h-20 w-full items-center justify-between bg-slate-200 p-4 dark:bg-slate-800">
         <div className="flex items-center gap-x-4">
-          <Image
-            src={LOGO_PATH}
-            className={{ common: "h-12 w-12 rounded-full" }}
-          />
+          <Tooltip content="Home">
+            <span
+              className="hover:cursor-pointer"
+              onClick={() => navigate(ROUTES.HOME.path)}
+            >
+              <Image
+                src={LOGO_PATH}
+                className={{ common: "h-12 w-12 rounded-full" }}
+              />
+            </span>
+          </Tooltip>
           {isLoggedIn && <NavbarSearch />}
         </div>
 
--- frontend/src/components/common/navbar/ThemeToggle.tsx
+++ frontend/src/components/common/navbar/ThemeToggle.tsx
@@ -3,6 +3,7 @@ import { useThemeContext } from "../../../contexts/ThemeProvider";
 import React from "react";
 import { MdSunny } from "react-icons/md";
 import { IoMoon } from "react-icons/io5";
+import Tooltip from "../Tooltip";
 
 export default function ThemeToggle() {
   const { theme, toggleTheme } = useThemeContext();
@@ -14,13 +15,17 @@ export default function ThemeToggle() {
       <IoMoon className="text-black" />
     );
 
+  const tooltipText = `Switch to ${theme === "light" ? "dark" : "light"} mode`;
+
   return (
-    <div className="bg-blue">
-      <Switch className="" onClick={toggleTheme} checked={theme === "dark"}>
-        <div className="flex h-full w-full items-center justify-center">
-          {component}
-        </div>
-      </Switch>
-    </div>
+    <Tooltip content={tooltipText}>
+      <span>
+        <Switch onClick={toggleTheme} checked={theme === "dark"}>
+          <div className="flex h-full w-full items-center justify-center">
+            {component}
+          </div>
+        </Switch>
+      </span>
+    </Tooltip>
   );
 }
