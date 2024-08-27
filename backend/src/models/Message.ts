2039b2195f02b9a610b193ad0c83f2bbd3816812
misat1505 <misat2002@gmail.com>
2024-08-27 11:58:01 +0200

    refactored isMessage

--- backend/src/models/Message.ts
+++ backend/src/models/Message.ts
@@ -18,19 +18,10 @@ export function isMessage(obj: any, options = defaultOptions): obj is Message {
   return (
     obj &&
     typeof obj === "object" &&
-    typeof obj.id === "string" &&
-    (typeof obj.content === "string" || obj.content === null) &&
-    isUser(obj.author, options) &&
-    (obj.createdAt instanceof Date ||
-      (options.allowStringifiedDates &&
-        typeof obj.createdAt === "string" &&
-        !isNaN(Date.parse(obj.createdAt)))) &&
     (obj.response === null || isPartialMessage(obj.response, options)) &&
-    typeof obj.chatId === "string" &&
-    Array.isArray(obj.files) &&
-    obj.files.every(isFile) &&
     Array.isArray(obj.reactions) &&
-    obj.reactions.every((reaction: unknown) => isReaction(reaction, options))
+    obj.reactions.every((reaction: unknown) => isReaction(reaction, options)) &&
+    isPartialMessage(obj, options)
   );
 }
 
