a844437fb62086d3e5b5dd014e897b0aed8ad2ad
misat1505 <misat2002@gmail.com>
2024-09-02 13:47:50 +0200

    better rws for login page

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -7,9 +7,9 @@ import LoginSlogan from "../components/login/LoginSlogan";
 
 export default function Login() {
   return (
-    <div className="min-h-[calc(100vh-5rem)] w-full grid-cols-2 px-12 xl:grid">
+    <div className="min-h-[calc(100vh-5rem)] w-full grid-cols-2 px-4 xl:grid xl:px-12">
       <LoginSlogan />
-      <div className="col-span-1 mx-auto my-auto h-fit w-96 rounded-lg bg-transparent px-4 py-8 shadow-2xl shadow-black">
+      <div className="col-span-1 mx-auto mb-4 h-fit w-96 max-w-full rounded-lg bg-transparent px-4 py-8 shadow-2xl shadow-black xl:my-auto">
         <LoginPageLogo />
         <LoginFormProvider>
           <LoginForm />
