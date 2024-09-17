b4617ac815a2af62aba0962adebbbb126abd693a
misat1505 <misat2002@gmail.com>
2024-09-17 13:13:36 +0200

    posts are exapndable

--- frontend/src/components/posts/PostPreview.tsx
+++ frontend/src/components/posts/PostPreview.tsx
@@ -1,24 +1,38 @@
 import MDEditor from "@uiw/react-md-editor";
 import { useThemeContext } from "../../contexts/ThemeProvider";
 import { Post } from "../../types/Post";
-import React from "react";
+import React, { useState } from "react";
 import { User } from "../../types/User";
 import Avatar from "../common/Avatar";
 import { buildFileURL } from "../../utils/buildFileURL";
 import { getInitials } from "../../utils/getInitials";
 import { timeDifference } from "../../utils/timeDifference";
+import { cn } from "../../lib/utils";
+import { Button } from "../ui/button";
 
 export default function PostPreview({ post }: { post: Post }) {
   const { theme } = useThemeContext();
+  const [isExpanded, setIsExpanded] = useState(false);
 
   return (
     <div
       data-color-mode={theme}
-      className="m-auto my-4 max-w-[60%] rounded-md p-4"
+      className={cn("m-auto my-4 max-w-[60%] overflow-hidden rounded-md p-4", {
+        "relative max-h-72": !isExpanded
+      })}
       style={{ backgroundColor: theme === "light" ? "white" : "#0c1117" }}
     >
       <PostAuthor user={post.author} />
       <MDEditor.Markdown source={post.content} />
+      {!isExpanded && (
+        <Button
+          variant="blueish"
+          className="absolute bottom-4 left-1/2 -translate-x-1/2"
+          onClick={() => setIsExpanded(true)}
+        >
+          Show more
+        </Button>
+      )}
     </div>
   );
 }
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,27 +1,12 @@
-import React, { MouseEvent } from "react";
+import React from "react";
 import Loading from "../components/common/Loading";
-import { useNavigate } from "react-router-dom";
 import { useAppContext } from "../contexts/AppProvider";
-import { User } from "../types/User";
-import Image from "../components/common/Image";
-import { ROUTES } from "../lib/routes";
-import { Button } from "../components/ui/button";
-import { AuthService } from "../services/Auth.service";
-import { buildFileURL } from "../utils/buildFileURL";
 import { useQuery } from "react-query";
 import { PostService } from "../services/Post.service";
 import PostPreview from "../components/posts/PostPreview";
 
 export default function Home() {
-  const { user, setUser } = useAppContext();
-  const navigate = useNavigate();
-
-  // const handleLogout = async (e: MouseEvent) => {
-  //   e.preventDefault();
-  //   await AuthService.logout();
-  //   setUser(null);
-  //   navigate(ROUTES.LOGIN.path);
-  // };
+  const { user } = useAppContext();
 
   const { data: posts, isLoading } = useQuery({
     queryKey: ["posts"],
@@ -31,37 +16,4 @@ export default function Home() {
   if (!user || isLoading) return <Loading />;
 
   return <div>{posts?.map((post) => <PostPreview post={post} />)}</div>;
-
-  // return (
-  //   <div className="flex w-full justify-between">
-  //     <div className="m-4 flex w-fit items-center gap-x-8 rounded-md bg-slate-200 p-4 dark:bg-slate-800">
-  //       <ProfileAvatar user={user} />
-  //       <p className="text-lg font-semibold">
-  //         {user.firstName} {user.lastName}
-  //       </p>
-  //     </div>
-  //     <Button variant="blueish" onClick={() => navigate(ROUTES.SETTINGS.path)}>
-  //       Settings
-  //     </Button>
-  //     <Button variant="blueish" onClick={handleLogout}>
-  //       logout
-  //     </Button>
-  //   </div>
-  // );
-}
-
-function ProfileAvatar({ user }: { user: User }) {
-  const initials = `${user.firstName[0].toUpperCase()}${user.lastName[0].toUpperCase()}`;
-
-  return (
-    <Image
-      src={buildFileURL(user.photoURL, { type: "avatar" })}
-      className={{
-        common: "h-12 w-12 rounded-full",
-        img: "object-cover",
-        error: "bg-white font-semibold dark:bg-black"
-      }}
-      errorContent={initials}
-    />
-  );
 }
