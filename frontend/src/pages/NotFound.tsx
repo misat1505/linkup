83c1577276c5f061b1c611bc535a334778403888
misat1505 <misat2002@gmail.com>
2024-10-07 16:08:50 +0200

    better not found page

--- frontend/src/pages/NotFound.tsx
+++ frontend/src/pages/NotFound.tsx
@@ -1,5 +1,27 @@
+import { useNavigate } from "react-router-dom";
+import Image from "../components/common/Image";
 import React from "react";
+import { LOGO_PATH } from "../constants";
+import { Button } from "../components/ui/button";
+import { ROUTES } from "../lib/routes";
 
 export default function NotFound() {
-  return <div>NotFound</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div className="relative h-[calc(100vh-5rem)]">
+      <div className="absolute left-1/2 top-1/2 flex max-w-80 -translate-x-1/2 -translate-y-1/2 flex-col items-center rounded-md bg-slate-200 p-4 dark:bg-slate-800">
+        <Image
+          src={LOGO_PATH}
+          className={{ common: "h-40 w-40 rounded-full object-cover" }}
+        />
+        <p className="mb-4 mt-6 text-center text-black dark:text-white">
+          Oops. It seems like you didn&apos;t want to go here.
+        </p>
+        <Button variant="blueish" onClick={() => navigate(ROUTES.HOME.path)}>
+          Bring me back home.
+        </Button>
+      </div>
+    </div>
+  );
 }
