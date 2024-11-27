30418728efe1fa43958b1224e9997000960618ad
misat1505 <misat2002@gmail.com>
2024-11-27 15:15:01 +0100

    created swagger docs

--- backend/src/controllers/friendships/createFriendship.controller.ts
+++ backend/src/controllers/friendships/createFriendship.controller.ts
@@ -2,6 +2,80 @@ import { Request, Response } from "express";
 import { FriendshipService } from "../../services/FriendshipService";
 
 export const createFriendship = async (req: Request, res: Response) => {
+  /**
+   * @swagger
+   * /friendships:
+   *   post:
+   *     summary: Create a new friendship
+   *     tags: [Friendships]
+   *     requestBody:
+   *       required: true
+   *       content:
+   *         application/json:
+   *           schema:
+   *             type: object
+   *             properties:
+   *               requesterId:
+   *                 type: string
+   *                 description: ID of the user requesting the friendship
+   *                 example: "3b6431d2-43a4-427d-9f28-ab9001ad4f63"
+   *               acceptorId:
+   *                 type: string
+   *                 description: ID of the user accepting the friendship request
+   *                 example: "9a2e94ad-604c-46ea-b96c-44c490d1a91a"
+   *     responses:
+   *       200:
+   *         description: Friendship created successfully
+   *         content:
+   *           application/json:
+   *             schema:
+   *               type: object
+   *               properties:
+   *                 friendship:
+   *                   type: object
+   *                   properties:
+   *                     status:
+   *                       type: string
+   *                       description: Current status of the friendship
+   *                       example: "PENDING"
+   *                     requester:
+   *                       description: Details of the requester user
+   *                       $ref: '#/components/schemas/User'
+   *                     acceptor:
+   *                       description: Details of the acceptor user
+   *                       $ref: '#/components/schemas/User'
+   *       400:
+   *         description: The requester ID does not match the user making the request
+   *         content:
+   *           application/json:
+   *             schema:
+   *               type: object
+   *               properties:
+   *                 message:
+   *                   type: string
+   *                   example: "Cannot create friendship not requested by you."
+   *       409:
+   *         description: Friendship already exists between users
+   *         content:
+   *           application/json:
+   *             schema:
+   *               type: object
+   *               properties:
+   *                 message:
+   *                   type: string
+   *                   example: "Friendship already exists between users."
+   *       500:
+   *         description: Server error while creating friendship
+   *         content:
+   *           application/json:
+   *             schema:
+   *               type: object
+   *               properties:
+   *                 message:
+   *                   type: string
+   *                   example: "Cannot get user friendships."
+   */
+
   try {
     const { userId } = req.body.token;
     const { requesterId, acceptorId } = req.body;
--- backend/src/controllers/friendships/deleteFriendship.controller.ts
+++ backend/src/controllers/friendships/deleteFriendship.controller.ts
@@ -2,6 +2,70 @@ import { Request, Response } from "express";
 import { FriendshipService } from "../../services/FriendshipService";
 
 export const deleteFriendship = async (req: Request, res: Response) => {
+  /**
+   * @swagger
+   * /friendships:
+   *   delete:
+   *     summary: Delete an existing friendship
+   *     tags: [Friendships]
+   *     requestBody:
+   *       required: true
+   *       content:
+   *         application/json:
+   *           schema:
+   *             type: object
+   *             properties:
+   *               requesterId:
+   *                 type: string
+   *                 description: ID of the user who sent the friendship request
+   *                 example: "3b6431d2-43a4-427d-9f28-ab9001ad4f63"
+   *               acceptorId:
+   *                 type: string
+   *                 description: ID of the user who accepted the friendship request
+   *                 example: "9a2e94ad-604c-46ea-b96c-44c490d1a91a"
+   *     responses:
+   *       200:
+   *         description: Friendship deleted successfully
+   *         content:
+   *           application/json:
+   *             schema:
+   *               type: object
+   *               properties:
+   *                 message:
+   *                   type: string
+   *                   example: "Friendship deleted successfully."
+   *       400:
+   *         description: The user is not authorized to delete this friendship
+   *         content:
+   *           application/json:
+   *             schema:
+   *               type: object
+   *               properties:
+   *                 message:
+   *                   type: string
+   *                   example: "Cannot delete friendship not belonging to you."
+   *       404:
+   *         description: Friendship not found
+   *         content:
+   *           application/json:
+   *             schema:
+   *               type: object
+   *               properties:
+   *                 message:
+   *                   type: string
+   *                   example: "Friendship not found."
+   *       500:
+   *         description: Server error while deleting friendship
+   *         content:
+   *           application/json:
+   *             schema:
+   *               type: object
+   *               properties:
+   *                 message:
+   *                   type: string
+   *                   example: "Cannot get user friendships."
+   */
+
   try {
     const { userId } = req.body.token;
     const { requesterId, acceptorId } = req.body;
