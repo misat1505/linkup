9779987968f7a35bd74ed62644b54e04a14f5f26
misat1505 <misat2002@gmail.com>
2024-12-19 11:36:04 +0100

    further copying

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ThemeProvider } from "./contexts/ThemeProvider.tsx";
+import BgGradient from "./components/common/BgGradient.tsx";
 
-createRoot(document.getElementById('root')!).render(
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+        <BgGradient />
+        <div className="relative z-10">
+          <App />
+        </div>
+      </ThemeProvider>
+    </QueryClientProvider>
+  </StrictMode>
+);
