b8d60ca6af07abc2b5356422b52c812bfc1b7ffe
misat1505 <misat2002@gmail.com>
2024-12-19 14:34:32 +0100

    deleted old frontend

--- frontend2/src/constants.ts
+++
@@ -1,8 +0,0 @@
-// URL to backend
-export const API_URL = process.env.REACT_APP_API_URL!;
-
-// URL to socket
-export const SOCKET_URL = process.env.REACT_APP_SOCKET_URL!;
-
-// path to app logo
-export const LOGO_PATH = "/assets/logo.webp";
--- frontend2/src/types/Reaction.ts
+++
@@ -1,9 +0,0 @@
-import { Message } from "./Message";
-import { User } from "./User";
-
-export type Reaction = {
-  id: string;
-  name: string;
-  messageId: Message["id"];
-  user: User;
-};
