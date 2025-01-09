f4da48c5d28cd1efa76ec419354ae915e74a2d91
misat1505 <misat2002@gmail.com>
2025-01-09 22:08:07 +0100

    changed cookie policy

--- backend/src/config/jwt-cookie.ts
+++ backend/src/config/jwt-cookie.ts
@@ -4,6 +4,8 @@ import { SignOptions } from "jsonwebtoken";
 export const refreshTokenCookieName = "refresh-token";
 
 export const refreshTokenCookieOptions: CookieOptions = {
+  sameSite: "none",
+  secure: true,
   httpOnly: true,
   maxAge: 7 * 24 * 60 * 60 * 1000, // 7d
 };
