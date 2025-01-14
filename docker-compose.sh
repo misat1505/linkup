68c0da794cd38e3732f63aa38f91f776b12fc6a7
misat1505 <misat2002@gmail.com>
2025-01-14 17:12:29 +0100

    e2e tests work

--- docker-compose.sh
+++ docker-compose.sh
@@ -7,11 +7,17 @@ command=$1
 case "$command" in
     prod)
         echo "Running application in production mode..."
-        docker compose -f ./docker/docker-compose.yml up --build
+        docker compose -f ./docker/docker-compose.yml up --build -d
+        cd backend
+        npm i
+        npm run start
         ;;
     e2e)
         echo "Running application for e2e testing..."
-        docker compose -f ./docker/docker-compose.e2e.yml up --build
+        docker compose -f ./docker/docker-compose.e2e.yml up --build -d
+        cd backend
+        npm i
+        npm run e2e
         ;;
     test)
         echo "Running application for backend testing..."
