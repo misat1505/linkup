4ca4830c979804132dc610d636a82c757ea69163
misat1505 <misat2002@gmail.com>
2025-04-27 15:00:14 +0200

    tests for validators

--- backend/tests/validators/auth.validators.test.ts
+++ backend/tests/validators/auth.validators.test.ts
@@ -1,200 +1,164 @@
-import { ValidationChain } from "express-validator";
-import express from "express";
 import { validate } from "../../src/middlewares/validate";
 import { loginRules, signupRules } from "../../src/validators/auth.validator";
-import request from "supertest";
-import i18next from "../../src/i18n";
-import middleware from "i18next-http-middleware";
-
-const createTestServer = (validations: ValidationChain[]) => {
-  const app = express();
-  app.use(express.json());
-  app.use(middleware.handle(i18next));
-  app.post("/test", validate(validations), (req, res) => {
-    res.status(200).send("Validation passed");
-  });
-  return app;
-};
+import { mockRequest, mockResponse } from "../utils/mocks";
 
 describe("Auth validators", () => {
   describe("Login validation rules", () => {
     it("passes with correct data", async () => {
-      const app = createTestServer(loginRules);
-      const res = await request(app).post("/test").send({
-        login: "login1",
-        password: "pass1",
-      });
+      const req = mockRequest({ body: { login: "login1", password: "pass1" } });
+      const res = mockResponse();
+      const mockNextFunction = jest.fn();
 
-      expect(res.statusCode).toBe(200);
-      expect(res.text).toBe("Validation passed");
+      const middleware = validate(loginRules);
+      await middleware(req, res, mockNextFunction);
+
+      expect(mockNextFunction).toHaveBeenCalled();
     });
 
     it("fails with data of bad types", async () => {
-      const app = createTestServer(loginRules);
-      const res = await request(app).post("/test").send({
-        login: 11111,
-        password: 22222,
-      });
+      const req = mockRequest({ body: { login: 11111, password: 22222 } });
+      const res = mockResponse();
+      const mockNextFunction = jest.fn();
 
-      expect(res.statusCode).toBe(400);
-      expect(res.body.errors).toEqual(
-        expect.arrayContaining([
-          expect.objectContaining({
-            msg: "Login has to be a string.",
-          }),
-          expect.objectContaining({
-            msg: "Password has to be a string.",
-          }),
-        ])
-      );
+      const middleware = validate(loginRules);
+      await middleware(req, res, mockNextFunction);
+
+      expect(mockNextFunction).not.toHaveBeenCalled();
+
+      const jsonCallArgs = (res.json as jest.Mock).mock.calls[0][0];
+      expect(jsonCallArgs.errors).toHaveLength(2);
     });
 
     it("fails with no data", async () => {
-      const app = createTestServer(loginRules);
-      const res = await request(app).post("/test").send({});
+      const req = mockRequest({ body: {} });
+      const res = mockResponse();
+      const mockNextFunction = jest.fn();
 
-      expect(res.statusCode).toBe(400);
-      expect(res.body.errors).toEqual(
-        expect.arrayContaining([
-          expect.objectContaining({ msg: "Login must be provided." }),
-          expect.objectContaining({ msg: "Password must be provided." }),
-        ])
-      );
+      const middleware = validate(loginRules);
+      await middleware(req, res, mockNextFunction);
+
+      expect(mockNextFunction).not.toHaveBeenCalled();
+
+      const jsonCallArgs = (res.json as jest.Mock).mock.calls[0][0];
+      expect(jsonCallArgs.errors).toHaveLength(2);
     });
 
     it("fails with data too short", async () => {
-      const app = createTestServer(loginRules);
-      const res = await request(app).post("/test").send({
-        login: "logi",
-        password: "pass",
-      });
+      const req = mockRequest({ body: { login: "logi", password: "pass" } });
+      const res = mockResponse();
+      const mockNextFunction = jest.fn();
 
-      expect(res.statusCode).toBe(400);
-      expect(res.body.errors).toEqual(
-        expect.arrayContaining([
-          expect.objectContaining({
-            msg: "Login must be between 5 and 50 characters long.",
-          }),
-          expect.objectContaining({
-            msg: "Password must be at least 5 characters long.",
-          }),
-        ])
-      );
+      const middleware = validate(loginRules);
+      await middleware(req, res, mockNextFunction);
+
+      expect(mockNextFunction).not.toHaveBeenCalled();
+
+      const jsonCallArgs = (res.json as jest.Mock).mock.calls[0][0];
+      expect(jsonCallArgs.errors).toHaveLength(2);
     });
 
     it("fails with data too long", async () => {
-      const app = createTestServer(loginRules);
-      const res = await request(app)
-        .post("/test")
-        .send({
-          login: "l".repeat(51),
-          password: "pass1",
-        });
+      const req = mockRequest({
+        body: { login: "l".repeat(51), password: "pass1" },
+      });
+      const res = mockResponse();
+      const mockNextFunction = jest.fn();
 
-      expect(res.statusCode).toBe(400);
-      expect(res.body.errors).toEqual(
-        expect.arrayContaining([
-          expect.objectContaining({
-            msg: "Login must be between 5 and 50 characters long.",
-          }),
-        ])
-      );
+      const middleware = validate(loginRules);
+      await middleware(req, res, mockNextFunction);
+
+      expect(mockNextFunction).not.toHaveBeenCalled();
+
+      const jsonCallArgs = (res.json as jest.Mock).mock.calls[0][0];
+      expect(jsonCallArgs.errors).toHaveLength(1);
     });
   });
 
   describe("Signup validation rules", () => {
     it("passes with correct data", async () => {
-      const app = createTestServer(signupRules);
-      const res = await request(app).post("/test").send({
-        firstName: "John",
-        lastName: "Doe",
-        login: "login1",
-        password: "pass1",
+      const req = mockRequest({
+        body: {
+          firstName: "John",
+          lastName: "Doe",
+          login: "login1",
+          password: "pass1",
+        },
       });
+      const res = mockResponse();
+      const mockNextFunction = jest.fn();
 
-      expect(res.statusCode).toBe(200);
-      expect(res.text).toBe("Validation passed");
+      const middleware = validate(signupRules);
+      await middleware(req, res, mockNextFunction);
+
+      expect(mockNextFunction).toHaveBeenCalled();
     });
 
     it("fails with no data", async () => {
-      const app = createTestServer(signupRules);
-      const res = await request(app).post("/test").send({});
+      const req = mockRequest({ body: {} });
+      const res = mockResponse();
+      const mockNextFunction = jest.fn();
 
-      expect(res.statusCode).toBe(400);
-      expect.arrayContaining([
-        expect.objectContaining({ msg: "First name must be provided." }),
-        expect.objectContaining({ msg: "Last name must be provided." }),
-        expect.objectContaining({ msg: "Login must be provided." }),
-        expect.objectContaining({ msg: "Password must be provided." }),
-      ]);
+      const middleware = validate(signupRules);
+      await middleware(req, res, mockNextFunction);
+
+      expect(mockNextFunction).not.toHaveBeenCalled();
+      const jsonCallArgs = (res.json as jest.Mock).mock.calls[0][0];
+      expect(jsonCallArgs.errors).toHaveLength(4);
     });
 
     it("fails with bad data types", async () => {
-      const app = createTestServer(signupRules);
-      const res = await request(app).post("/test").send({
-        firstName: 11111,
-        lastName: 22222,
-        login: 33333,
-        password: 44444,
+      const req = mockRequest({
+        body: {
+          firstName: 11111,
+          lastName: 22222,
+          login: 33333,
+          password: 44444,
+        },
       });
+      const res = mockResponse();
+      const mockNextFunction = jest.fn();
 
-      expect(res.statusCode).toBe(400);
-      expect.arrayContaining([
-        expect.objectContaining({ msg: "First name must be a string." }),
-        expect.objectContaining({ msg: "Last name must be a string." }),
-        expect.objectContaining({ msg: "Login must be a string." }),
-        expect.objectContaining({ msg: "Password must be a string." }),
-      ]);
+      const middleware = validate(signupRules);
+      await middleware(req, res, mockNextFunction);
+
+      expect(mockNextFunction).not.toHaveBeenCalled();
+      const jsonCallArgs = (res.json as jest.Mock).mock.calls[0][0];
+      expect(jsonCallArgs.errors).toHaveLength(4);
     });
 
     it("fails with too short data", async () => {
-      const app = createTestServer(signupRules);
-      const res = await request(app).post("/test").send({
-        firstName: "",
-        lastName: "",
-        login: "",
-        password: "",
+      const req = mockRequest({
+        body: { firstName: "", lastName: "", login: "", password: "" },
       });
+      const res = mockResponse();
+      const mockNextFunction = jest.fn();
 
-      expect(res.statusCode).toBe(400);
-      expect.arrayContaining([
-        expect.objectContaining({ msg: "First name must not be empty." }),
-        expect.objectContaining({ msg: "Last name must not be empty." }),
-        expect.objectContaining({
-          msg: "Login must be at least 5 characters long.",
-        }),
-        expect.objectContaining({
-          msg: "Password must be at least 5 characters long.",
-        }),
-      ]);
+      const middleware = validate(signupRules);
+      await middleware(req, res, mockNextFunction);
+
+      expect(mockNextFunction).not.toHaveBeenCalled();
+      const jsonCallArgs = (res.json as jest.Mock).mock.calls[0][0];
+      expect(jsonCallArgs.errors).toHaveLength(4);
     });
 
     it("fails with too long data", async () => {
-      const app = createTestServer(signupRules);
-      const res = await request(app)
-        .post("/test")
-        .send({
+      const req = mockRequest({
+        body: {
           firstName: "a".repeat(51),
           lastName: "a".repeat(51),
           login: "a".repeat(51),
           password: "pass1",
-        });
+        },
+      });
+      const res = mockResponse();
+      const mockNextFunction = jest.fn();
 
-      expect(res.statusCode).toBe(400);
-      expect.arrayContaining([
-        expect.objectContaining({
-          msg: "Fist name must be at most 50 characters long.",
-        }),
-        expect.objectContaining({
-          msg: "Last name must be at most 50 characters long.",
-        }),
-        expect.objectContaining({
-          msg: "Login must be at least 5 characters long.",
-        }),
-        expect.objectContaining({
-          msg: "Password must be at least 5 characters long.",
-        }),
-      ]);
+      const middleware = validate(signupRules);
+      await middleware(req, res, mockNextFunction);
+
+      expect(mockNextFunction).not.toHaveBeenCalled();
+      const jsonCallArgs = (res.json as jest.Mock).mock.calls[0][0];
+      expect(jsonCallArgs.errors).toHaveLength(3);
     });
   });
 });
