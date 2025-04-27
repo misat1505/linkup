643e909ff71a21eeb7145e09b30ba2397cbb2846
misat1505 <misat2002@gmail.com>
2025-04-27 17:35:53 +0200

    split user controllers

---
+++ backend/tests/controllers/user/search-user.test.ts
@@ -0,0 +1,53 @@
+import { UserControllers } from "../../../src/controllers";
+import { mockRequest, mockResponse, mockUserService } from "../../utils/mocks";
+import { seedProvider } from "../../utils/seedProvider";
+
+describe("searchUser", () => {
+  it("should return users", async () => {
+    await seedProvider(async (seed) => {
+      const users = seed.users;
+      mockUserService.searchUsers.mockResolvedValue(users);
+
+      const req = mockRequest({
+        query: { term: "abc" },
+      });
+
+      const res = mockResponse();
+
+      await UserControllers.searchUser(req, res, jest.fn());
+
+      expect(res.status).toHaveBeenCalledWith(200);
+      expect(res.json).toHaveBeenCalledWith({ users });
+    });
+  });
+
+  it("fails if term not provided", async () => {
+    await seedProvider(async (seed) => {
+      const users = seed.users;
+      mockUserService.searchUsers.mockResolvedValue(users);
+
+      const req = mockRequest({ query: {} });
+      const res = mockResponse();
+
+      await UserControllers.searchUser(req, res, jest.fn());
+
+      expect(res.status).toHaveBeenCalledWith(400);
+    });
+  });
+
+  it("fails if term provided more than once", async () => {
+    await seedProvider(async (seed) => {
+      const users = seed.users;
+      mockUserService.searchUsers.mockResolvedValue(users);
+
+      const req = mockRequest({
+        query: { term: ["abc", "dbc"] },
+      });
+      const res = mockResponse();
+
+      await UserControllers.searchUser(req, res, jest.fn());
+
+      expect(res.status).toHaveBeenCalledWith(400);
+    });
+  });
+});
