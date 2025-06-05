4f384dd28800d580a7a89118b4d4f1efc0fc3d0d
misat1505 <misat2002@gmail.com>
2025-06-05 10:44:48 +0200

    updated docker-compose.sh

--- docker-compose.sh
+++ docker-compose.sh
@@ -8,9 +8,6 @@ case "$command" in
     prod)
         echo "Running application in production mode..."
         docker compose -f ./docker/docker-compose.yml up --build
-        # cd backend
-        # npm i
-        # npm run start
         ;;
     e2e)
         echo "Running application for e2e testing..."
@@ -24,7 +21,7 @@ case "$command" in
         docker compose -f ./docker/docker-compose.yml up database socket minio --build
         ;;
     *)
-        echo "Invalid command. Use 'prod', 'e2e', 'test', 'dev'."
+        echo "Invalid command. Use 'prod', 'e2e', 'dev'."
         exit 1
         ;;
 esac
