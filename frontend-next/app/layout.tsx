e6b2eddf2a42ee2e35e9d2a2120abd248e263118
misat1505 <misat2002@gmail.com>
2026-03-02 17:46:51 +0100

    feat: theme switch

--- frontend-next/app/layout.tsx
+++ frontend-next/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import BgGradient from "@/components/shared/BgGradient";
 import { LanguageProvider } from "@/providers/LanguageProvider";
+import { ThemeProvider } from "@/providers/ThemeProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,14 +26,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <LanguageProvider>
-          <BgGradient />
-          <div className="relative z-10">{children}</div>
-        </LanguageProvider>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <LanguageProvider>
+            <BgGradient />
+            <div className="relative z-10">{children}</div>
+          </LanguageProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
--- frontend-next/app/page.tsx
+++ frontend-next/app/page.tsx
@@ -1,10 +1,14 @@
 import { I18nText } from "@/components/shared/I18nText";
 import { Button } from "@/components/ui/button";
+import { Switch } from "@/components/ui/theme-switch";
 
 export default function Home() {
   return (
-    <Button>
-      <I18nText translationKey="tabs.chats" values={{ name: "bbb" }} />
-    </Button>
+    <div>
+      <Switch />
+      <Button>
+        <I18nText translationKey="tabs.chats" values={{ name: "bbb" }} />
+      </Button>
+    </div>
   );
 }
