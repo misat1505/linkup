f10739686232f3c51cb542aac0e76c85dd5f669a
misat1505 <misat2002@gmail.com>
2026-03-04 13:51:42 +0100

    fix: fixed theme switches not synchronizing

--- frontend-next/components/ui/theme-switch.tsx
+++ frontend-next/components/ui/theme-switch.tsx
@@ -30,6 +30,7 @@ const Switch = React.forwardRef<
     <Tooltip content={tooltipText}>
       <span>
         <SwitchPrimitives.Root
+          checked={theme === "dark"}
           className={cn(
             "peer inline-flex h-6 w-11 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50",
             theme === "dark"
