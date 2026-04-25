30e49d61beb56812691b8be482488a7d75a708e5
misat1505 <misat2002@gmail.com>
2026-04-25 18:35:14 +0200

    fix: try to fix the build

--- apps/web-next/next.config.ts
+++ apps/web-next/next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -16,6 +17,13 @@ const nextConfig: NextConfig = {
         // pathname: "/linkup-bucket/**",
       },
     ],
+  },
+  webpack: (config) => {
+    config.resolve.alias["@packages"] = path.resolve(
+      __dirname,
+      "../../packages",
+    );
+    return config;
   },
 };
 
