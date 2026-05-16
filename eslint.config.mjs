7778539a131ab7b2bed37b7f4f5127a1c0d1ed67
misat1505 <misat2002@gmail.com>
2026-05-16 16:51:15 +0200

    feat: initialized prettier for the project

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,14 @@
 import js from "@eslint/js";
+import prettier from "eslint-config-prettier";
 import ts from "typescript-eslint";
 
 export default [
-  js.configs.recommended,
-  ...ts.configs.recommended,
-  {
-    rules: {
-      "no-console": ["warn", { allow: ["warn", "error", "info", "debug"] }],
-    },
-  },
+	js.configs.recommended,
+	...ts.configs.recommended,
+	{
+		rules: {
+			"no-console": ["warn", { allow: ["warn", "error", "info", "debug"] }],
+		},
+	},
+	prettier,
 ];
--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,7 +1,8 @@
 export default {
-  "apps/web-react/**/*.{js,jsx,ts,tsx}": () => "pnpm --filter web-react lint",
-  "apps/web-next/**/*.{js,jsx,ts,tsx}": () => "pnpm --filter web-next lint",
-  "apps/backend/**/*.{js,ts}": () => "pnpm --filter backend lint",
-  "apps/websocket/**/*.{js,ts}": () => "pnpm --filter websocket lint",
-  "packages/**/*.{js,ts,tsx}": () => "pnpm -r --filter './packages/**' lint",
+	"apps/web-react/**/*.{js,jsx,ts,tsx}": [() => "pnpm --filter web-react lint", "prettier --write"],
+	"apps/web-next/**/*.{js,jsx,ts,tsx}": [() => "pnpm --filter web-next lint", "prettier --write"],
+	"apps/backend/**/*.{js,ts}": [() => "pnpm --filter backend lint", "prettier --write"],
+	"apps/websocket/**/*.{js,ts}": [() => "pnpm --filter websocket lint", "prettier --write"],
+	"packages/**/*.{js,ts,tsx}": [() => "pnpm -r --filter './packages/**' lint", "prettier --write"],
+	"**/*.{json,md,yaml,yml,css}": ["prettier --write"],
 };
